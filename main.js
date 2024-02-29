b0c12c2f8d12cef516b3ba3a5a7d735937ba3f09
Shaiyur Dooken <shaiyur101@gmail.com>
2024-02-29 18:30:34 +0200

    Created app that shows a child window after 3 seconds

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const {app, BrowserWindow} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow, secondaryWindow;
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow () {
@@ -11,29 +11,45 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1000, height: 800,
     webPreferences: {
-      // --- !! IMPORTANT !! ---
-      // Disable 'contextIsolation' to allow 'nodeIntegration'
-      // 'contextIsolation' defaults to "true" as from Electron v12
       contextIsolation: false,
       nodeIntegration: true
     }
-  })
+  });
+
+  secondaryWindow = new BrowserWindow({
+    width: 300, height: 300,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  });
 
   // Load index.html into the new BrowserWindow
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open DevTools - Remove for PRODUCTION!
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Listen for window being closed
   mainWindow.on('closed',  () => {
       // debugger is basically a keyword for a breakpoint
         mainWindow = null
   })
+  // Listen for window being closed
+  secondaryWindow.on('closed',  () => {
+      // debugger is basically a keyword for a breakpoint
+        secondaryWindow = null
+  })
 }
 
 // Electron `app` is ready
 app.on('ready', createWindow)
+
+app.on('browser-window-focus', () => {
+  setTimeout(() => {
+    secondaryWindow.loadFile('index.html')
+  }, 3000);
+})
 
 // Quit when all windows are closed - (Not macOS - Darwin)
 app.on('window-all-closed', () => {
